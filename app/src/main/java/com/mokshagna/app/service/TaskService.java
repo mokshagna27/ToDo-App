88a268e5e0cae37607416d8f01a498b66823ef3a
Mokshagna Reddy <c.mokshagna27@gmail.com>
2025-10-24 13:15:22 +0530

    Created get all tasks

---
+++ app/src/main/java/com/mokshagna/app/service/TaskService.java
@@ -0,0 +1,20 @@
+package com.mokshagna.app.service;
+
+import com.mokshagna.app.models.Task;
+import com.mokshagna.app.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
